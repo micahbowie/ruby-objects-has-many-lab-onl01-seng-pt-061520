9554cc75f0cda6951ace1c696a35e988059c3dfa
Micah Bowie <micahbowie20@gmail.com>
2020-07-21 13:37:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,20 @@
-class Artis 
+class Artist 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name =name 
+  end 
+  
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    add_song(song)
+  end 
+  
+  def add_song(song)
+    song.artist=self 
+  end 
+  
+end 
+
   
   
