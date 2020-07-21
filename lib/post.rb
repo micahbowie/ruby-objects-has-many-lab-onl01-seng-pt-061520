1dff5f627c5da0ed4b8afb408ebcecea89ce6dc7
Micah Bowie <micahbowie20@gmail.com>
2020-07-21 14:31:26 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post 
+  attr_accessor :title, :genre, :artist
+  
+  @@all = []
+  
+  def initialize(title)
+    @title=title
+    save
+  end 
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+end 
