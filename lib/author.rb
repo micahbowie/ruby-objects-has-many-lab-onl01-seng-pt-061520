3737f1189fc515358f3eb150089b02d7d751e1d6
Micah Bowie <micahbowie20@gmail.com>
2020-07-21 14:26:40 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,7 +6,7 @@ class Author
   end 
   
   def add_post_by_titl(name)
-    song = Song.new(name)
+    blog = Post.new(name)
     add_song(song)
   end 
   
@@ -19,9 +19,9 @@ class Author
     the_list.select {|song| song.artist == self }
   end 
   
-  def self.song_count
+  def self.post_count
     @@song_artist_list =[]
-    the_list = Song.all
+    the_list = Post.all
     the_list.each {|song| @@song_artist_list << song.artist }
     lets_count = @@song_artist_list.uniq
     lets_count.count 
